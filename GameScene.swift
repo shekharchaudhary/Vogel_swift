ef7fce79769b345be25ee4168139fd1a879baa1b
shekharchaudhary <shekharchaudhary@users.noreply.github.com>
2017-01-01 23:39:41 -0800

    Add files via upload

--- GameScene.swift
+++ GameScene.swift
@@ -1,9 +1,9 @@
 //
 //  GameScene.swift
-//  Vogel
+//  vogel2
 //
-//  Created by Shekhar Chaudhary on 12/28/16.
-//  Copyright © 2016 Shekhar Chaudhary. All rights reserved.
+//  Created by Shekhar Chaudhary on 1/1/17.
+//  Copyright © 2017 Shekhar Chaudhary. All rights reserved.
 //
 
 import SpriteKit
@@ -11,26 +11,5 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    
-        
-        // 1
-        //let player = SKSpriteNode(imageNamed: "bird1_sm")
-        //let player1 = SKSpriteNode(imageNamed: "sun")
-        //let player2 = SKSpriteNode(imageNamed: "wave2")
-    
-        
-        override func didMove(to view: SKView) {
-            // 2
-            backgroundColor = SKColor.red
-            // 3
-            //player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
-            
-            //player1.position = CGPoint(x: size.width * 0.2, y: size.height*0.5)
-            // 4
-            //addChild(player)
-        }
-       
-    
-    }
-    
 
+}
--- gameviewcontroller2.swift
+++ gameviewcontroller2.swift
@@ -1,18 +1,22 @@
 //
-//  GameViewController.swift
+//  gameviewcontroller2.swift
 //  vogel2
 //
 //  Created by Shekhar Chaudhary on 1/1/17.
 //  Copyright © 2017 Shekhar Chaudhary. All rights reserved.
 //
 
+import Foundation
 import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
+class gameviewcontroller2: UIViewController {
     
-    @IBAction func view1(_ sender: UIButton) {
-        print("view1")
+    
+    @IBAction func a1(_ sender: UIButton) {
+    
+        print("hello")
+        
     }
 }
